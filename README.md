2d78811b67aee17a14d9d459010bf87ac4e9c141
Michael Whitener <mjawhitener@gmail.com>
2019-03-10 17:10:02 -0500

    Added support for ping and traceroute tests for win/unix.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rtrMon
 Pull information from routers through http/https, run ping tests, and trace routes.
+
+### Dependancies
+* traceroute for linux if it is not installed by default
--- rtrMon.py
+++ rtrMon.py
@@ -6,6 +6,9 @@
 import urllib.request as request
 import json
 import sys
+import os
+import subprocess
+
 
 project = sys.argv[1]
 host = sys.argv[2]
@@ -14,31 +17,59 @@ username = ''
 password = ''
 adminpwd = ''
 rtrResponce = ''
+posix = True
+pingResponce = ''
+traceRouteResponce = ''
 
-# pull login info from json file.
-with open('./login_info.json') as loginInfo:
-    data = json.load(loginInfo)
-    username = data[project]["username"]
-    password = data[project]["password"]
-    if "adminpwd" in data[project]:
-        adminpwd = data[project]["adminpwd"]
+# test what OS this is run on.
+if os.name == 'nt':
+    unix = False
 
-passman = request.HTTPPasswordMgrWithDefaultRealm()
-passman.add_password(None, host, username, password)
-authhandler = request.HTTPBasicAuthHandler(passman)
-opener = request.build_opener(authhandler)
 
-# url will differ between routers and may need more then one to pull all the information
-# needed so where and what will be stored in a seperate json file containing a list of
-# routers which can be modified if you need more/less information gathered.
-with open('./routers.json') as routers:
-    data = json.load(routers)
-    routerInfo = data[router]
-    for info in routerInfo:
-        responce = opener.open("http://" + host + info[0])
-        rtrResponce = responce.read()  # testing reply
-        responce.close()
+# perform a ping test
+def ping(host):
+    if posix:
+        output = subprocess.run(['ping', '-c', '1', host], capture_output=True)
+    else:
+        output = subprocess.run(['ping', '-n', '20', host], capture_output=True)
+    return str(output)[str(output).find('---'):str(output).find('stderr') - 3]
 
-print(rtrResponce)
 
-# TODO: add support for .asp sites
+# perform a traceroute
+def traceRoute(host):
+    if posix:
+        output = subprocess.run(['traceroute', host], capture_output=True)
+    else:
+        output = subprocess.run(['tracert', host], capture_output=True)
+    return str(output)[str(output).find('stdout') + 9:str(output).find('stderr') - 3]
+
+
+# # pull login info from json file.
+# with open('./login_info.json') as loginInfo:
+#     data = json.load(loginInfo)
+#     username = data[project]["username"]
+#     password = data[project]["password"]
+#     if "adminpwd" in data[project]:
+#         adminpwd = data[project]["adminpwd"]
+
+# passman = request.HTTPPasswordMgrWithDefaultRealm()
+# passman.add_password(None, host, username, password)
+# authhandler = request.HTTPBasicAuthHandler(passman)
+# opener = request.build_opener(authhandler)
+
+# # url will differ between routers and may need more then one to pull all the information
+# # needed so where and what will be stored in a seperate json file containing a list of
+# # routers which can be modified if you need more/less information gathered.
+# with open('./routers.json') as routers:
+#     data = json.load(routers)
+#     routerInfo = data[router]
+#     for info in routerInfo:
+#         responce = opener.open("http://" + host + info[0])
+#         rtrResponce = responce.read()  # testing reply
+#         responce.close()
+
+
+# for line in ping(host).split('\\n'):
+#     print(line)
+for line in traceRoute(host).split('\\n'):
+    print(line)
