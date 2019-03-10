5fff1c1636c3626ef0f1e523a922ba49b6cea16f
Michael Whitener <mjawhitener@gmail.com>
2019-03-10 12:37:24 -0500

    First commit, works with html pages only.

--- rtrMon.py
+++ rtrMon.py
@@ -1,6 +1,7 @@
 # rtrMon crawls through routers to fetch monitoring information.
 # sys.argv[1] = project
 # sys.argv[2] = host
+# sys.argv[3] = router
 
 import urllib.request as request
 import json
@@ -8,13 +9,15 @@ import sys
 
 project = sys.argv[1]
 host = sys.argv[2]
+router = sys.argv[3]
 username = ''
 password = ''
 adminpwd = ''
+rtrResponce = ''
 
 # pull login info from json file.
-with open('./login_info.json') as info:
-    data = json.load(info)
+with open('./login_info.json') as loginInfo:
+    data = json.load(loginInfo)
     username = data[project]["username"]
     password = data[project]["password"]
     if "adminpwd" in data[project]:
@@ -28,3 +31,12 @@ opener = request.build_opener(authhandler)
 # url will differ between routers and may need more then one to pull all the information
 # needed so where and what will be stored in a seperate json file containing a list of
 # routers which can be modified if you need more/less information gathered.
+with open('./routers.json') as routers:
+    data = json.load(routers)
+    routerInfo = data[router]
+    for info in routerInfo:
+        responce = opener.open("http://" + host + info[0])
+        rtrResponce = responce.read()  # testing reply
+        responce.close()
+
+print(rtrResponce)
